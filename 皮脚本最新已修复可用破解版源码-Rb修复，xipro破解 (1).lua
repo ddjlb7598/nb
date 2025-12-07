b291851a474ae9ecb59cfb3d079645a8e88ebcd0
ddjlb7598 <q18342117598@163.com>
2025-12-07 21:09:57 +0800

    Update 皮脚本最新已修复可用破解版源码-Rb修复，xipro破解 (1).lua

--- 皮脚本最新已修复可用破解版源码-Rb修复，xipro破解 (1).lua
+++ 皮脚本最新已修复可用破解版源码-Rb修复，xipro破解 (1).lua
@@ -1,14 +1,15 @@
 local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
-    Title = "迪脚本 Script",
+    Title = "tx脚本 Script",
     Text = "迪公告系列",
     Icon = "rbxassetid://82031063194606",
     Duration = 6, 
 })
 CoreGui:SetCore("SendNotification", {
-    Title = "正在加载迪脚本",
+    Title = "正在加载tx脚本",
     Text = "自动翻译",
     Icon = "rbxassetid://82031063194606",
     Duration = 6, 
 })
 loadstring(game:HttpGet("https://pastefy.app/TbI0vupa/raw"))()
+
